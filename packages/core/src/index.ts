41a0dcb75806ddb3541d64e4a6ddfcc9fef7b2d2
ctrotech-tutor <ctrotech.devs@gmail.com>
2026-02-10 00:11:15 +0100

    chore: release v4.0.0 stable

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -5,4 +5,4 @@ export * from './utils/Logger';
 export * from './core/validate';
 export * from './rules';
 export * as rules from './rules';
-export const version = '4.0.0-alpha.0';
+export const version = '4.0.0';
